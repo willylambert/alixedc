5c37e2a5bff51dde7a9dc6002530dc2e8b6365ab
Willy LAMBERT <willy.lambert@prodigia.org>
2012-05-30 12:01:30 +0200

    code cleaning

--- inc/class.bocdiscoo.inc.php
+++ inc/class.bocdiscoo.inc.php
@@ -1,7 +1,7 @@
 <?php
     /**************************************************************************\
     * ALIX EDC SOLUTIONS                                                       *
-    * Copyright 2011 Business & Decision Life Sciences                         *
+    * Copyright 2012 Business & Decision Life Sciences                         *
     * http://www.alix-edc.com                                                  *
     * ------------------------------------------------------------------------ *                                                                       *
     * This file is part of ALIX.                                               *
@@ -26,14 +26,9 @@ define("ODM_NAMESPACE","http://www.cdisc.org/ns/odm/v1.3");
 
 class bocdiscoo extends CommonFunctions
 {
-  var $m_lang; //lang de l'utilisateur : cf constructeur
-
-  //Constructeur
   function bocdiscoo(&$tblConfig,$ctrlRef)
   {
       CommonFunctions::__construct($tblConfig,$ctrlRef);
-
-      $this->m_lang = $GLOBALS['egw_info']['user']['preferences']['common']['lang'];
   }
 
 /*
